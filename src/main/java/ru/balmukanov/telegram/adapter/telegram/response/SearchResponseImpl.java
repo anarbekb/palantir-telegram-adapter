b071c591b27975b6c560c40cdb28785926787a22
anarbek <balmukanovab@gmail.com>
2021-11-16 15:00:42 +0500

    change text

--- src/main/java/ru/balmukanov/telegram/adapter/telegram/response/SearchResponseImpl.java
+++ src/main/java/ru/balmukanov/telegram/adapter/telegram/response/SearchResponseImpl.java
@@ -35,7 +35,7 @@ public class SearchResponseImpl implements SearchResponse {
 
 	private String getMessageText(SearchResult result) {
 		if (result.isFind()) {
-			StringBuilder builder = new StringBuilder("Нашлось " + result.getFinds().size() + " рез.:\n");
+			StringBuilder builder = new StringBuilder();
 			result.getFinds().forEach((host, link) -> builder.append(link).append("\n"));
 			return builder.toString();
 		} else {
